d1feda7cbee2a8b4f8605cb6fa0ea9ff7d5188e7
Robert Kaszubowski <robert_kaszubowski@epam.com>
2017-05-11 08:58:26 +0200

    small refactoring

--- src/test/java/com/taplayground/fluentlennium/tests/SimpleTest.java
+++ src/test/java/com/taplayground/fluentlennium/tests/SimpleTest.java
@@ -1,17 +1,11 @@
 package com.taplayground.fluentlennium.tests;
 
 import com.taplayground.fluentlennium.utils.UrlProvider;
-import org.junit.Before;
 import org.junit.Test;
 
 public class SimpleTest extends BaseFluentTest{
 
-    private String url;
-
-    @Before
-    public void setup() {
-        url = UrlProvider.SPORTSBOOK_COM.getUrl();
-    }
+    private String url = UrlProvider.SPORTSBOOK_COM.getUrl();
 
     @Test
     public void OpenBrowser() {
